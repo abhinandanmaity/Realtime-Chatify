974640206267b2221639faed590dbcd72677fea9
Abhinandan Maity <abhinandanmaity222@gmail.com>
2024-02-29 20:30:32 +0530

    new commit

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,57 +1,57 @@
-// import { getServerSession } from 'next-auth'
-import { getToken } from 'next-auth/jwt'
-import { withAuth } from 'next-auth/middleware'
-import { NextResponse } from 'next/server'
-// import { useDispatch } from 'react-redux'
-// import { useremail } from '@/redux/slice/user/sessionSlice';
-// import { authOptions } from './app/api/auth/[...nextauth]/route'
+// // import { getServerSession } from 'next-auth'
+// import { getToken } from 'next-auth/jwt'
+// import { withAuth } from 'next-auth/middleware'
+// import { NextResponse } from 'next/server'
+// // import { useDispatch } from 'react-redux'
+// // import { useremail } from '@/redux/slice/user/sessionSlice';
+// // import { authOptions } from './app/api/auth/[...nextauth]/route'
 
-export default withAuth(
-    async function middleware(req) {
-    //     const dispatch = useDispatch()
-    // dispatch(useremail());
-        const pathname = req.nextUrl.pathname
-        // const session = await getServerSession(authOptions);
-        // console.log("session ",session)
+// export default withAuth(
+//     async function middleware(req) {
+//     //     const dispatch = useDispatch()
+//     // dispatch(useremail());
+//         const pathname = req.nextUrl.pathname
+//         // const session = await getServerSession(authOptions);
+//         // console.log("session ",session)
 
-        // Manage route protection
-        const isAuth = await getToken({ req })
-        // console.log(isAuth.email)
-        const isLoginPage = pathname.startsWith('/sign-in')
-        const isSignupPage = pathname.startsWith('/sign-up')
+//         // Manage route protection
+//         const isAuth = await getToken({ req })
+//         // console.log(isAuth.email)
+//         const isLoginPage = pathname.startsWith('/sign-in')
+//         const isSignupPage = pathname.startsWith('/sign-up')
 
-        const sensitiveRoutes = ['/user']
-        const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
-            pathname.startsWith(route)
-        )
+//         const sensitiveRoutes = ['/user']
+//         const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
+//             pathname.startsWith(route)
+//         )
 
-        if (isLoginPage || isSignupPage) {
-            if (isAuth) {
-                return NextResponse.redirect(new URL('/user', req.url))
-            }
+//         if (isLoginPage || isSignupPage) {
+//             if (isAuth) {
+//                 return NextResponse.redirect(new URL('/user', req.url))
+//             }
 
-            return NextResponse.next()
-        }
+//             return NextResponse.next()
+//         }
 
-        if (!isAuth && isAccessingSensitiveRoute) {
-            return NextResponse.redirect(new URL('/sign-in', req.url))
-        }
-        if (pathname === '/' && isAuth) {
-            return NextResponse.redirect(new URL('/user', req.url))
-        }
-        if (pathname === '/' && !isAuth) {
-            return NextResponse.redirect(new URL('/sign-in', req.url))
-        }
-    },
-    {
-        callbacks: {
-            async authorized() {
-                return true
-            },
-        },
-    }
-)
+//         if (!isAuth && isAccessingSensitiveRoute) {
+//             return NextResponse.redirect(new URL('/sign-in', req.url))
+//         }
+//         if (pathname === '/' && isAuth) {
+//             return NextResponse.redirect(new URL('/user', req.url))
+//         }
+//         if (pathname === '/' && !isAuth) {
+//             return NextResponse.redirect(new URL('/sign-in', req.url))
+//         }
+//     },
+//     {
+//         callbacks: {
+//             async authorized() {
+//                 return true
+//             },
+//         },
+//     }
+// )
 
-export const config = {
-    matcher: ["/", "/sign-in", "/sign-up", "/user", "/user/conversations/:path*"]
-}
+// export const config = {
+//     matcher: ["/", "/sign-in", "/sign-up", "/user", "/user/conversations/:path*"]
+// }
