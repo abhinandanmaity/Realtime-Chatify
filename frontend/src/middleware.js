beca4f95e8455177ac75e8c7cf443a756d6efcde
Abhinandan Maity <abhinandanmaity222@gmail.com>
2024-03-01 18:11:09 +0530

    c37

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -18,7 +18,8 @@ export default withAuth(
 
         // Manage route protection
         const secret = process.env.NEXTAUTH_SECRET;
-        const isAuth = await getToken({ req: req })
+        // const isAuth = await getToken({ req: req })
+        const isAuth = await getToken({ req: req, secret: secret });
         // console.log(isAuth?.email)
         const isLoginPage = pathname.startsWith('/sign-in')
         const isSignupPage = pathname.startsWith('/sign-up')
@@ -35,7 +36,8 @@ export default withAuth(
         console.log(" ---- req.cookies ---- ")
         console.log(req.cookies?.get('__Secure-next-auth.session-token')?.value)
         // const isAuth = jwt?.verify(req.cookies?.get('__Secure-next-auth.session-token')?.value, secret)
-        // console.log(isAuth)
+        console.log(isAuth)
+        console.log(secret)
 
         // const decode = authOptions?.decode;
         // console.log(decode)
