0a97f9981c2423081f5e083f5fe5f5704402fbbb
Abhinandan Maity <abhinandanmaity222@gmail.com>
2024-03-01 17:42:33 +0530

    c36

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -33,12 +33,12 @@ export default withAuth(
         // console.log(isAuth_)
         // console.log(req.cookies)
         console.log(" ---- req.cookies ---- ")
-        // console.log(req.cookies?.get('__Secure-next-auth.session-token')?.value)
+        console.log(req.cookies?.get('__Secure-next-auth.session-token')?.value)
         // const isAuth = jwt?.verify(req.cookies?.get('__Secure-next-auth.session-token')?.value, secret)
         // console.log(isAuth)
 
-        const decode = authOptions?.decode;
-        console.log(decode)
+        // const decode = authOptions?.decode;
+        // console.log(decode)
 
         // console.log(req.nextUrl.pathname == "/sign-in")
         const sensitiveRoutes = ['/user']
